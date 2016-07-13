69a83c578c9bd921f415ee79a93e7b8f7e3e67ae
Adam Nawrocki <adam.nawroc@gmail.com>
2016-07-13 12:03:54 -0400

    put results in center of the page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function() {
-  var body = $("body");
+  var div = $("div");
 
   var to_this = prompt("Pick an integer greater than 0 to fizz: ");
 
@@ -20,14 +20,14 @@ for (var i = 1; i <= to_this; i++) {
   if (i % 3 === 0) {
     if (i % 5 === 0)
      {
-      body.append("<p>Fizzbuzz</p>");
+      div.append("<p>Fizzbuzz</p>");
      } else {
-        body.append("<p>fizz</p>");
+        div.append("<p>fizz</p>");
      }
   } else if (i % 5 === 0) {
-    body.append("<p>buzz</p>");
+    div.append("<p>buzz</p>");
   } else {
-    body.append("<p>" + i + "</p>");
+    div.append("<p>" + i + "</p>");
   }
 
 }
