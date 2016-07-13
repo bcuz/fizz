0455807978d5bad6b80f76b82993929758e22fbc
Adam Nawrocki <adam.nawroc@gmail.com>
2016-07-13 06:51:11 -0400

    donezo

--- js/script.js
+++ js/script.js
@@ -4,12 +4,14 @@ for (var i = 0; i <= 30; i++) {
     if (i % 5 === 0)
      {
       $("body").append("<p>Fizzbuzz</p>");
+     } else {
+        $("body").append("<p>" + "fizz" + "</p>");
      }
   } else if (i % 5 === 0) {
     $("body").append("<p>" + "buzz" + "</p>");
+  } else {
+    $("body").append("<p>" + i + "</p>");
   }
-
-
 
 }
 
