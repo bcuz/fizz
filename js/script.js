adbcb9ce5a5e53c11de1aadf897cc201a3e763cb
Adam Nawrocki <adam.nawroc@gmail.com>
2016-07-13 06:49:58 -0400

    account for 3,5 and 5

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 $(function() {
-for (var i = 0; i <= 5; i++) {
-  $("body").append("<p>" + i + "</p>");
+for (var i = 0; i <= 30; i++) {
+  if (i % 3 === 0) {
+    if (i % 5 === 0)
+     {
+      $("body").append("<p>Fizzbuzz</p>");
+     }
+  } else if (i % 5 === 0) {
+    $("body").append("<p>" + "buzz" + "</p>");
+  }
+
+
+
 }
 
 });
